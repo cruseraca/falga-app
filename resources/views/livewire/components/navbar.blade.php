08d65fae0973053d04ba04991373fabf7cbf47cc
Syahrian Adil <syahrianadil22@gmail.com>
2022-09-18 23:32:41 +0700

    update navbar

---
+++ resources/views/livewire/components/navbar.blade.php
@@ -0,0 +1,42 @@
+<div>
+    <nav class="flex items-center justify-between flex-wrap bg-white-500 py-5 px-12">
+        <div class="flex items-center h-12 mr-6">
+            <img class="object-cover relative h-full" src="{{ asset('img/logo-falga.svg') }}" alt="">
+        </div>
+        <div class="block lg:hidden">
+            <button
+                class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+                <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                    <title>Menu</title>
+                    <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+                </svg>
+            </button>
+        </div>
+        <div class="w-full block lg:flex lg:items-center lg:w-auto  justify-items-center">
+            <div class="text-md lg:flex-grow">
+                <a href="#responsive-header"
+                    class="block mt-4 lg:inline-block lg:mt-0 text-purple-falga hover:text-white mx-4">
+                    About Us
+                </a>
+                <a href="#responsive-header"
+                    class="block mt-4 lg:inline-block lg:mt-0 text-black hover:text-purple-falga mx-4">
+                    Our Business
+                </a>
+                <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-black hover:text-purple-falga mx-4">
+                    ESG
+                </a>
+                <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-black hover:text-purple-falga mx-4">
+                    Media
+                </a>
+                <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-black hover:text-purple-falga mx-4">
+                    Career
+                </a>
+            </div>
+        </div>
+        <div>
+            <a href="#"
+                class="inline-block text-sm px-4 py-2 leading-none border rounded-[8px] bg-purple-falga text-white border-white hover:border-purple-falga hover:text-purple-falga hover:bg-white mt-4 lg:mt-0">Contact
+                Us</a>
+        </div>
+    </nav>
+</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,23 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require('tailwindcss/defaultTheme');
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
     ],
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+                sans: ["Istok Web", ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                "purple-falga": "#2b4290",
             },
         },
     },
 
-    plugins: [require('@tailwindcss/forms')],
+    plugins: [require("@tailwindcss/forms")],
 };
