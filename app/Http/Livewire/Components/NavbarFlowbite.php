e0f5820b003d3b983d9fa0b0dba5f94604457ca9
Syahrian Adil <syahrianadil22@gmail.com>
2022-11-06 22:27:43 +0700

    fix component not found

---
+++ app/Http/Livewire/Components/NavbarFlowbite.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Components;
+
+use Livewire\Component;
+
+class NavbarFlowbite extends Component
+{
+    public function render()
+    {
+        return view('livewire.components.navbar-flowbite');
+    }
+}
