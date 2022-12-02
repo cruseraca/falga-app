150bb7217edf24d481d892fc4c989ea410a18d5d
Kukuh Yoniatmoko <kukuhyoniatmoko@gmail.com>
2022-12-02 15:54:05 +0700

    perbaikan

--- resources/views/livewire/our-business-page.blade.php
+++ resources/views/livewire/our-business-page.blade.php
@@ -1,11 +1,11 @@
 {{-- Banner --}}
 <div class="sm:w-full bg-our-business-banner bg-no-repeat bg-cover bg-top flex mt-[61px] md:mt-[77px]">
     <div class="container text-center h-[400px] lg:h-[766px] text-white flex flex-col justify-end items-center mx-auto" style="margin-bottom: 80px">
-        <h1 class="font-poppins font-extrabold text-xl sm:text-2xl lg:text-4xl lg:leading-relaxed uppercase" style="text-shadow: 0px 4px 4px #000000;">
+        <h1 class="font-poppins font-extrabold text-xl sm:text-2xl lg:text-4xl lg:leading-relaxed uppercase" style="text-shadow: 0px 1px 1px rgba(0, 0, 0, 0.7);">
             Building Agility, Resilience and Sustainability
             <br />Our Businesses
         </h1>
-        <div class="mt-8 normal-case md:text-2xl" style="text-shadow: 0px 4px 4px #000000;">
+        <div class="mt-8 normal-case md:text-2xl" style="text-shadow: 0px 1px 1px rgba(0, 0, 0, 0.7);">
             FALGA is an Indonesian-incorporated Investment Holding Company that strives to continuously build viable
             large-scale businesses that deliver sustainable performance
         </div>
@@ -16,18 +16,16 @@
 [ "title" => "OUR BUSINESS", "color" => "red-falga" ],
 ]])
 @endcomponent
-<div class="falga-container flex flex-col md:flex-row md:my-10">
-    <div class="flex relative h-[300px] md:h-[576px] md:basis-1/2">
-        <div class="w-[80%] m-auto md:ml-24">
-            <img src="{{ asset('img/our-business/epci.jpg') }}" alt="">
-        </div>
-        <div class="absolute -z-50 top-8 right-4 md:top-14 md:right-0 bg-gradient-to-b from-indigo-900 to to-pink-700 h-[80%] w-2/5"></div>
+<div class="falga-container flex flex-col md:flex-row md:items-center py-8 md:py-16">
+    <div class="flex relative h-[250px] md:h-[416px] my-3 md:my-5">
+        <img class="w-[288px] h-[211px] md:w-[480px] md:h-[352px] object-cover my-[19px] ml-[19px] md:my-[32px] md:ml-[32px]" src="{{ asset('img/our-business/epci.png') }}" alt="">
+        <div class="absolute -z-50 top-0 left-0 bg-[#595897] w-[233px] md:w-[388px] h-full"></div>
     </div>
-    <div class="font-poppins py-8 md:basis-1/2 md:p-16">
-        <div class="text-xl text-rose-800 font-black mb-4 md:text-3xl">ENGINEERING, PROCUREMENT, CONSTRUCTION AND INSTALLMENT (EPCI)</div>
-        <div class="text-justify md:text-lg">FALGA has robust track record in EPCI projects of more than 110 EPC Infrastructure Projects in Indonesia partnering with Indonesian SOEs and Public/Private Companies since 1986. We have accumulated extensive project experience in infrastructure: LPG Tank Farm, Oil/Fuel Farm, Electric Motor, Jetty & Piers, Pipelines & Piping System, Fuel Gas & LPG Filling Station, etc</div>
-        <div class="text-center my-4 md:mt-10 flex justify-center md:justify-start">
-            <a href="{{ url('epci-business') }}" class="falga-button uppercase text-xs md:ml-0">See All</a>
+    <div class="flex-1 md:ml-12 my-3 md:my-5">
+        <div class="falga-section-title red-falga mb-4 md:mb-8">ENGINEERING, PROCUREMENT, CONSTRUCTION AND INSTALLMENT (EPCI)</div>
+        <div class="text-justify text-sm md:text-base mb-4 md:mb-8">FALGA has robust track record in EPCI projects of more than 110 EPC Infrastructure Projects in Indonesia partnering with Indonesian SOEs and Public/Private Companies since 1986. We have accumulated extensive project experience in infrastructure: LPG Tank Farm, Oil/Fuel Farm, Electric Motor, Jetty & Piers, Pipelines & Piping System, Fuel Gas & LPG Filling Station, etc</div>
+        <div class="text-center flex justify-center md:justify-start">
+            <a href="{{ url('epci-business') }}" class="falga-button uppercase text-xs">See All</a>
         </div>
     </div>
 </div>
@@ -35,14 +33,14 @@
     <div class="falga-container flex flex-col -z-9 md:pt-10">
         <div class="flex relative h-[200px] md:h-[576px] md:basis-1/2">
             <div class="w-[80%] md:h-[400px] md:w-[500px] m-auto z-10">
-                <img class="object-cover object-right md:h-full" src="{{ asset('img/our-business/oil_gas.jpg') }}" alt="">
+                <img class="object-cover object-right md:h-full" src="{{ asset('img/our-business/oil-gas.png') }}" alt="">
             </div>
-            <div class="absolute -bottom-1 left-6 md:-bottom-8 md:left-[27%] bg-gradient-to-b from-indigo-900 to to-pink-700 h-[70%] w-[80%] md:w-[35%] md:h-[90%]"></div>
+            <div class="absolute -bottom-1 left-6 md:-bottom-8 md:left-[27%] bg-[#A55067] h-[70%] w-[80%] md:w-[35%] md:h-[90%]"></div>
         </div>
         <div class="font-poppins py-8 md:basis-1/2 md:p-16">
             <div class="text-xl text-rose-800 font-black mb-4 md:text-3xl text-center">OIL & GAS RETAIL</div>
             <div class="text-justify md:text-lg">FALGA owns and manages Fuel Gas Station (SPBU) and LPG Cylinder Retester Station (BPT LPG) across Indonesia compliant to the operational standards set by PERTAMINA as the Oil & Gas Indonesian SOE. Our Group continuously focuses on business process improvement in our existing stations and assesses potential new Oil & Gas Retail businesses.</div>
-            <div class="text-center my-4 md:mt-10 flex justify-center">
+            <div class="text-center my-4 md:mt-8 flex justify-center">
                 <a href="{{ url('oil-and-gas-business') }}" class="falga-button uppercase text-xs md:mx-auto">See All</a>
             </div>
         </div>
@@ -58,7 +56,7 @@
     <div class="font-poppins py-8 md:basis-1/2 md:p-16">
         <div class="text-xl text-rose-800 font-black mb-4 md:text-3xl md:text-right">INDUSTRIAL EQUIPMENT RENTAL</div>
         <div class="text-justify md:text-lg">FALGA provides industrial machinery and construction equipment rental services with functionality and quality. Our group offers wide assortment of industrial tools from ordinary machines like Generator Set to Specialized tools like Jacking System. We also offer full-service delivery by providing routine maintenance by our technicians on the construction equipment and tool rentals.</div>
-        <div class="my-4 md:mt-10 md:float-right flex md:justify-end justify-center">
+        <div class="my-4 md:mt-8 md:float-right flex md:justify-end justify-center">
             <a href="{{ url('industrial-rental') }}" class="falga-button uppercase text-xs md:ml-0">See All</a>
         </div>
     </div>
@@ -74,7 +72,7 @@
         <div class="font-poppins py-8 md:basis-1/2 md:p-16">
             <div class="text-xl text-rose-800 font-black mb-4 md:text-3xl">PROPERTY AND HOSPITALITY</div>
             <div class="text-justify md:text-lg">FALGA owns and manages boarding houses and budget lodges that offer convenient, comfortable and affordable stay for our guests. As the demand for board housing increases in Indonesia, we strive to offer well-maintained facility and to continuously upgrade the housing quality by integrating smart technology.</div>
-            <div class="text-center my-4 md:mt-10 flex md:justify-start justify-center">
+            <div class="text-center my-4 md:mt-8 flex md:justify-start justify-center">
                 <a href="{{ url('prop-business') }}" class="falga-button uppercase text-xs md:ml-0">See All</a>
             </div>
         </div>
@@ -90,7 +88,7 @@
     <div class="font-poppins py-8 md:basis-1/2 md:py-16 md:px-24">
         <div class="text-xl text-rose-800 font-black mb-4 md:text-3xl text-center">FUTURE BUSINESS</div>
         <div class="text-justify md:text-lg">FALGA proactively explore and appraise new businesses that can drive sustainable values. Our group positions our management strategy to be agile to market movement and to be open to new business prospects.</div>
-        <div class="text-center my-4 md:mt-10 flex justify-center">
+        <div class="text-center my-4 md:mt-8 flex justify-center">
             <a href="{{ url('future-business') }}" class="falga-button uppercase text-xs md:mx-auto">See All</a>
         </div>
     </div>
