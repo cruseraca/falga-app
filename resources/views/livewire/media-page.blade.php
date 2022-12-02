f7cf80f467a0c15c80a9419a24d210bbe2b53c7c
Kukuh Yoniatmoko <kukuhyoniatmoko@gmail.com>
2022-12-02 10:34:05 +0700

    add filter

--- resources/views/livewire/media-page.blade.php
+++ resources/views/livewire/media-page.blade.php
@@ -1,6 +1,9 @@
 @push('styles')
 <link href="{{ asset('css/media-and-information.css') }}" rel="stylesheet">
 @endpush
+@push('scripts')
+<script src="https://unpkg.com/flowbite@1.5.4/dist/datepicker.js"></script>
+@endpush
 {{-- Banner --}}
 <div class="sm:w-full falga-banner bg-no-repeat bg-center bg-cover flex mt-[61px] md:mt-[77px] items-center justify-center">
     <h1 class="font-poppins text-white my-[212px] md:my-[312px] text-center font-extrabold lg:text-4xl lg:leading-relaxed uppercase" style="text-shadow: 0px 4px 4px #000000;">
@@ -33,7 +36,50 @@
         </div>
     </div>
     <div id="esg-tab-content" class="flex-1 md:border-l">
-        <div class="falga-section-title red-falga mt-8 py-3 border-b-2 border-gray-600 md:ml-8 md:px-8">{{ $title }}</div>
+        <div class="flex flex-wrap md:ml-8 md:px-4 mt-6 py-3">
+            <button id="location-bt" data-dropdown-toggle="location" class="mt-2 mx-1 md:mx-2 bg-gray-50 border border-gray-300 text-gray-900 text-base md:text-xl rounded-lg focus:ring-red-falga focus:border-red-falga block p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-red-falga dark:focus:border-red-falga flex items-baseline" type="button">
+                <div class="flex-1 mx-6">Location</div> <i class="fa-solid fa-chevron-down"></i>
+            </button>
+            <!-- Dropdown menu -->
+            <div id="location" class="hidden z-10 w-255 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 border">
+                <ul class="py-1 text-xl text-gray-700 dark:text-gray-200" aria-labelledby="location-bt">
+                    <li>
+                        <a class="block py-2 px-4 hover:bg-gray-300 dark:hover:bg-gray-600 dark:hover:text-white">Makasar, Indonesian</a>
+                    </li>
+                    <li>
+                        <a class="block py-2 px-4 hover:bg-gray-300 dark:hover:bg-gray-600 dark:hover:text-white">Jayapura, Indonesian</a>
+                    </li>
+                    <li>
+                        <a class="block py-2 px-4 hover:bg-gray-300 dark:hover:bg-gray-600 dark:hover:text-white">Ngawi, Indonesia</a>
+                    </li>
+                    <li>
+                        <a class="block py-2 px-4 hover:bg-gray-300 dark:hover:bg-gray-600 dark:hover:text-white">Maumere, Indoensia</a>
+                    </li>
+                </ul>
+            </div>
+            <button id="category-bt" data-dropdown-toggle="category" class="mt-2 mx-1 md:mx-2 bg-gray-50 border border-gray-300 text-gray-900 text-base md:text-xl rounded-lg focus:ring-red-falga focus:border-red-falga block p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-red-falga dark:focus:border-red-falga flex items-center" type="button">
+                <div class="flex-1 mx-6">Category</div> <i class="fa-solid fa-chevron-down"></i>
+            </button>
+            <!-- Dropdown menu -->
+            <div id="category" class="hidden z-10 w-255 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 border">
+                <ul class="py-1 text-xl text-gray-700 dark:text-gray-200" aria-labelledby="category-bt">
+                    <li>
+                        <a class="block py-2 px-4 hover:bg-gray-300 dark:hover:bg-gray-600 dark:hover:text-white">Category</a>
+                    </li>
+                </ul>
+            </div>
+            {{-- <select id="category" class="mx-1 md:mx-2 bg-gray-50 md:min-w-32 border border-gray-300 text-gray-900 text-base md:text-xl rounded-lg focus:ring-red-falga focus:border-red-falga block p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-red-falga dark:focus:border-red-falga">
+                <option selected>Category</option>
+                <option value="epci">EPCI Buisnesss</option>
+            </select> --}}
+            <div class="relative mt-2 mx-1 md:mx-2">
+                <div class="flex absolute inset-y-0 right-0 items-center pr-3 pointer-events-none">
+                    <i class="fa-regular fa-calendar-days"></i>
+                    {{-- <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg> --}}
+                </div>
+                <input datepicker datepicker-format="M, dd yyyy" type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-base md:text-xl rounded-lg focus:ring-red-falga focus:border-red-falga block w-full pl-10 p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Select date">
+            </div>
+        </div>
         <div class="flex flex-wrap items-stretch justify-center">
             @if($q == 'portfolio')
             @foreach($items as $item)
