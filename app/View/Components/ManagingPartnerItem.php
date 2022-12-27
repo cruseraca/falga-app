1a88b9acbd760321eefbe10914c18184a005e0a9
Kukuh Yoniatmoko <kukuhyoniatmoko@gmail.com>
2022-12-27 14:30:41 +0700

    change key person naratives

--- app/View/Components/ManagingPartnerItem.php
+++ app/View/Components/ManagingPartnerItem.php
@@ -6,13 +6,29 @@ use Illuminate\View\Component;
 
 class ManagingPartnerItem extends Component
 {
+    /**
+     * @var string
+     */
     public $name;
+    /**
+     * @var string
+     */
     public $position;
+    /**
+     * @var string
+     */
     public $image;
+    /**
+     * @var string
+     */
     public $description;
     /**
      * Create a new component instance.
      *
+     * @param string $name
+     * @param string $position
+     * @param string $image
+     * @param string $description
      * @return void
      */
     public function __construct($name, $position, $image, $description)
--- resources/views/components/managing-partner-item.blade.php
+++ resources/views/components/managing-partner-item.blade.php
@@ -1,10 +1,12 @@
 <div class="falga-mp-item mt-12 md:mt-14">
-    <div class="falga-mp-item-c h-full flex-col md:flex-row items-center">
-        <img data-src="{{ $image }}" class="lozad falga-mp-item-i"></img>
-        <div class="md:flex-1 flex flex-col justify-center">
-            <div class="falga-mp-item-title uppercase text-red-falga">{{ $name }}</div>
-            <div class="falga-mp-item-position">{{ $position }}</div>
-            <div class="mt-4 text-justify falga-mp-item-description">{{ $description }}</div>
-        </div>
+  <div class="falga-mp-item-c h-full flex-col md:flex-row items-center">
+    <img data-src="{{ $image }}" class="lozad falga-mp-item-i"></img>
+    <div class="md:flex-1 flex flex-col justify-center">
+      <div class="falga-mp-item-title uppercase text-red-falga">{{ $name }}</div>
+      @if($position && !empty($position))
+      <div class="falga-mp-item-position">{{ $position }}</div>
+      @endif
+      <div class="mt-4 text-justify falga-mp-item-description">{{ $description }}</div>
     </div>
+  </div>
 </div>
