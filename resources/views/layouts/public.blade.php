e96097dee877b73f40f5c780569cf183b4e45fa5
Kukuh Yoniatmoko <kukuhyoniatmoko@gmail.com>
2022-12-29 16:32:12 +0700

    optimize font

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -1,40 +1,46 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta name="csrf-token" content="{{ csrf_token() }}" />
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <meta name="csrf-token" content="{{ csrf_token() }}" />
 
-    <title>{{ config("app.name", "Falga") }}</title>
-    @section('meta_description')
-    <meta name="description" content="Core Emphasis on Business Building through Sustainable Investing">
-    @show
+  <title>{{ config("app.name", "Falga") }}</title>
+  @section('meta_description')
+  <meta name="description" content="Core Emphasis on Business Building through Sustainable Investing">
+  @show
 
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <!-- Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  {{-- <link href="https://fonts.googleapis.com/css2?family=Istok+Web:wght@400;700&family=Poppins:wght@300;400;600;700&display=swap" rel="stylesheet" /> --}}
+
+  <link rel="preload" href="https://fonts.googleapis.com/css2?family=Istok+Web:wght@400;700&family=Poppins:wght@300;400;600;700&display=swap" as="style" onload="this.onload=null;this.rel='stylesheet'" />
+  <noscript>
     <link href="https://fonts.googleapis.com/css2?family=Istok+Web:wght@400;700&family=Poppins:wght@300;400;600;700&display=swap" rel="stylesheet" />
+  </noscript>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
 
-    <!-- Styles -->
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}" />
-    <link rel="stylesheet" href="{{ asset('flickity/flickity.css') }}" />
+  <!-- Scripts -->
+  <script src="{{ asset('js/app.js') }}" defer></script>
 
-    <!-- push target to head -->
-    @stack('styles')
+  <!-- Styles -->
+  <link rel="stylesheet" href="{{ asset('css/app.css') }}" />
+  <link rel="stylesheet" href="{{ asset('flickity/flickity.css') }}" />
+
+  <!-- push target to head -->
+  @stack('styles')
 </head>
 
 <body>
-    @livewire('components.navbar-flowbite')
-    <div>
-        {{ $slot }}
-    </div>
-    @livewire('components.footer')
-    <script src="{{ asset('js/jquery.min.js') }}"></script>
-    <script src="https://unpkg.com/flowbite@1.5.3/dist/flowbite.js"></script>
-    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/lozad/dist/lozad.min.js"></script>
-    @stack('scripts')
+  @livewire('components.navbar-flowbite')
+  <div>
+    {{ $slot }}
+  </div>
+  @livewire('components.footer')
+  <script src="{{ asset('js/jquery.min.js') }}"></script>
+  <script src="https://unpkg.com/flowbite@1.5.3/dist/flowbite.js"></script>
+  <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/lozad/dist/lozad.min.js"></script>
+  @stack('scripts')
 </body>
 </html>
