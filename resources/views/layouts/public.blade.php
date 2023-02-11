5247729e4a8ae0c6a4b1fd8f35cf94cf5790d858
Kukuh Yoniatmoko <kukuhyoniatmoko@gmail.com>
2023-02-11 16:06:18 +0700

    add gtag scrypt

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -47,6 +47,16 @@
     }
   </script>
 
+  <!-- Google tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-3DQCF443F2"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-3DQCF443F2');
+  </script>
+
   <!-- Fonts -->
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
