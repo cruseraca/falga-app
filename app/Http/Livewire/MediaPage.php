254742a129222b62f76604921928218244613c01
Kukuh Yoniatmoko <kukuhyoniatmoko@gmail.com>
2023-04-13 21:28:22 +0700

    add news

--- app/Http/Livewire/MediaPage.php
+++ app/Http/Livewire/MediaPage.php
@@ -35,40 +35,46 @@ class MediaPage extends Component
         $this->all_items = [
             "news" => [
                 [
-                    "title" => "FALGA Group Supports Elementary School Education in Jayapura",
-                    "description" => "PT. Favor Alpha Omega (FALGA) Group through the GNOTA Foundation has helped 100 foster children, built school facilities and infrastructure and built clean water wells to support student learning activities at SD INPRESS Kuipons Jayapura.",
-                    "image" => mix("img/media-and-information/news/falga-gnota.jpg"),
-                    "url" => "http://www.gn-ota.or.id/en/falga-group-supports-elementary-school-education-in-jayapura/",
-                ],
-                [
-                    "title" => "Falga Group Ungkap Urgensi Konstruksi Berkelanjutan",
+                    "title" => "CNBC Indonesia: Falga Group Ungkap Urgensi Konstruksi Berkelanjutan",
                     "description" => "Falga Group mencoba untuk berperan mendorong dan meningkatkan proyek Sustainable Construction yang berkelanjutan.",
                     "image" => mix("img/media-and-information/news/cnbc.jpeg"),
                     "url" => "https://www.cnbcindonesia.com/news/20230410202452-4-428716/falga-group-ungkap-urgensi-konstruksi-berkelanjutan",
                 ],
                 [
-                    "title" => "Falga Group Dorong Pembangunan dengan Konsep Konstruksi Berkelanjutan",
+                    "title" => "CNN Indonesia: Falga Group Dorong Pembangunan dengan Konsep Konstruksi Berkelanjutan",
                     "description" => "Falga Group mendorong pembangunan dengan konsep sustainable construction agar sesuai standar ESG sekaligus membantu pencapaian Net Zero Emission (NZE) 2060.",
                     "image" => mix("img/media-and-information/news/cnn.jpeg"),
                     "url" => "https://www.cnnindonesia.com/ekonomi/20230411204126-97-936435/falga-group-dorong-pembangunan-dengan-konsep-konstruksi-berkelanjutan",
                 ],
                 [
-                    "title" => "Wujudkan Nett Zero Emision 2060 Falga Group Terapkan Sustainable Construction",
+                    "title" => "Detik: Upaya Falga Group Dorong Proyek Konstruksi Berkelanjutan di RI",
+                    "description" => "Sektor konstruksi memegang peranan penting dalam roda perekonomian nasional Tanah Air, di samping konsumsi dan manufaktur.",
+                    "image" => mix("img/media-and-information/news/detik.jpeg"),
+                    "url" => "https://finance.detik.com/infrastruktur/d-6666858/upaya-falga-group-dorong-proyek-konstruksi-berkelanjutan-di-ri",
+                ],
+                [
+                    "title" => "Kontan: Wujudkan Nett Zero Emision 2060 Falga Group Terapkan Sustainable Construction",
                     "description" => "Sektor konstruksi menjadi salah satu penopang perekonomian nasional Indonesia, selain konsumsi dan manufaktur.",
                     "image" => mix("img/media-and-information/news/kontan.jpg"),
                     "url" => "https://industri.kontan.co.id/news/wujudkan-nett-zero-emision-2060-falga-group-terapkan-sustainable-construction",
                 ],
                 [
-                    "title" => "Pembangunan Konstruksi Berkelanjutan di Jakarta Terus Didorong",
+                    "title" => "IDX: Wujudkan Net Zero Emision 2060, Falga Group Terapkan Sustainable Construction",
+                    "description" => "Pembangunan di Indonesia bisa sesuai dengan standar Environmental, Social and Governance (ESG) sekaligus membantu pencapaian Net Zero Emission (NZE) 2060.",
+                    "image" => mix("img/media-and-information/news/idx.jpg"),
+                    "url" => "https://www.idxchannel.com/economics/wujudkan-net-zero-emision-2060-falga-group-terapkan-sustainable-construction",
+                ],
+                [
+                    "title" => "Republika: Pembangunan Konstruksi Berkelanjutan di Jakarta Terus Didorong",
                     "description" => "Falga Group terus mendorong untuk pembangunan konstruksi berkelanjutan di Jakarta.",
                     "image" => mix("img/media-and-information/news/republika.jpg"),
                     "url" => "https://news.republika.co.id/berita/rsy9fx330/pembangunan-konstruksi-berkelanjutan-di-jakarta-terus-didorong",
                 ],
                 [
-                    "title" => "Upaya Falga Group Dorong Proyek Konstruksi Berkelanjutan di RI",
-                    "description" => "Sektor konstruksi memegang peranan penting dalam roda perekonomian nasional Tanah Air, di samping konsumsi dan manufaktur.",
-                    "image" => mix("img/media-and-information/news/detik.jpeg"),
-                    "url" => "https://finance.detik.com/infrastruktur/d-6666858/upaya-falga-group-dorong-proyek-konstruksi-berkelanjutan-di-ri",
+                    "title" => "FALGA Group Supports Elementary School Education in Jayapura",
+                    "description" => "PT. Favor Alpha Omega (FALGA) Group through the GNOTA Foundation has helped 100 foster children, built school facilities and infrastructure and built clean water wells to support student learning activities at SD INPRESS Kuipons Jayapura.",
+                    "image" => mix("img/media-and-information/news/falga-gnota.jpg"),
+                    "url" => "http://www.gn-ota.or.id/en/falga-group-supports-elementary-school-education-in-jayapura/",
                 ],
             ],
             "blog" => [
@@ -95,6 +101,18 @@ class MediaPage extends Component
             ],
             "video" => [
                 [
+                    "title" => "Wujudkan Konstruksi Ramah Lingkungan Lewat Sustainable Construction",
+                    "description" => "Indonesia terus mendorong pembangunan berkelanjutan sebagai bagian dari komitmen Net Zero Carbo 20260. Salah satu pengembangan \"Sustainability In The Construction Ecosystem\" Tanah Air dilaksanakan melalui program penyelenggaraan konstruksi nasional dalam pembangunan Ibu Kota Nusantara (IKN).",
+                    "image" => mix("img/hero-image.jpg"),
+                    "video_id" => "aC1KdyQyKp8"
+                ],
+                [
+                    "title" => "Sustainable Construction, Masa Depan Konstruksi Indonesia",
+                    "description" => "Istilah 'konstruksi berkelanjutan' semakin populer seiring pembangunan Ibu Kota Negara atau IKN yang melibatkan alam, teknologi dan energi terbarukan.",
+                    "image" => mix("img/hero-image.jpg"),
+                    "video_id" => "GTBgclI8P0Q"
+                ],
+                [
                     "title" => "Commissioning Gas in LPG Pressurized Terminal Jayapura 2x1000 MT Pertamina",
                     "description" => "1st LPG Terminal in Papua.\nCommissioning Gas In - LPG Pressurized Terminal in Jayapura 2x1000 MT & Jetty 6500 DWT.",
                     "image" => mix("img/hero-image.jpg"),
@@ -108,7 +126,7 @@ class MediaPage extends Component
                 ],
                 [
                     "title" => "Simbolisasi & Kunjungan Edukasi Gabriel dari Papua",
-                    "description" => "Gabriel dan Bapak Sujono datang dari Jayapura ke Jakarta dalam rangka menghadiri simbolisasi penyerahan donasi anak asuh dari OTA @falga.group di kantor Yayasan GNOTA.\nKegiatan dilanjutkan dengan kunjungan edukasi ke tempat-tempat ikonik di Jakarta seperti Monas, Kota Tua, Museum Keramik, dan lain sebagainya.\nYuk lihat keseruan Gabriel dan Bapak Sujono selama 3 hari di Jakarta.",
+                    "description" => "Gabriel dan Bapak Sujono datang dari Jayapura ke Jakarta dalam rangka menghadiri simbolisasi penyerahan donasi anak asuh dari OTA @falga.group di kantor Yayasan GNOTA.",
                     "image" => mix("img/hero-image.jpg"),
                     "video_id" => "_56KLyUQ1ms"
                 ],
@@ -124,18 +142,6 @@ class MediaPage extends Component
                     "image" => mix("img/hero-image.jpg"),
                     "video_id" => "u4kRsp7tvWk"
                 ],
-                [
-                    "title" => "Wujudkan Konstruksi Ramah Lingkungan Lewat Sustainable Construction",
-                    "description" => "Indonesia terus mendorong pembangunan berkelanjutan sebagai bagian dari komitmen Net Zero Carbo 20260. Salah satu pengembangan \"Sustainability In The Construction Ecosystem\" Tanah Air dilaksanakan melalui program penyelenggaraan konstruksi nasional dalam pembangunan Ibu Kota Nusantara (IKN).\nCEO & Managing Partner Falga Group, Ronald Edy Simamora mengatakan aplikasi Sustainability In The Construction di Indonesia memasuki tahap awal. Falga Group dalam menerapkan pembangunan Sustainability In The Construction dilaksanakan lewat penggunaan material emisi rendah hingga material yang tahan ledakan untuk mengurangi efek sosial dan sistem otomasi untuk mengoptimalkan energi yang rendah emisi.\nSeperti apa urgensi penerapan Sustainable Construction dalam pembangunan di Indonesia? Selengkapnya saksikan dialog Safrina Nasution dengan CEO & Managing Partner Falga Group, Ronald Edy Simamora dalam Evening Up,CNBC Indonesia (Selasa, 11 /04/2023)",
-                    "image" => mix("img/hero-image.jpg"),
-                    "video_id" => "aC1KdyQyKp8"
-                ],
-                [
-                    "title" => "Sustainable Construction, Masa Depan Konstruksi Indonesia",
-                    "description" => "Istilah 'konstruksi berkelanjutan' semakin populer seiring pembangunan Ibu Kota Negara atau IKN yang melibatkan alam, teknologi dan energi terbarukan.\nNamun sebenarnya seperti apa potensi Indonesia mengaplikasikan konstruksi berkelanjutan? Dan apakah Indonesia telah siap menopang implementasi konstruksi berkelanjutan? Selengkapnya akan dibahas dalam pemaparan Dina Gurning: Sustainable Construction, Masa Depan Konstruksi Indonesia dalam Evening Up, CNBC  Indonesia (Selasa, 11 /04/2023)",
-                    "image" => mix("img/hero-image.jpg"),
-                    "video_id" => "GTBgclI8P0Q"
-                ]
             ],
         ];
         /** @var \Illuminate\Http\Request $request */
@@ -150,7 +156,7 @@ class MediaPage extends Component
         if (!empty($request->input('page')) && intval($request->input('page'))) {
             $this->page = intval($request->input('page'));
         }
-        $count = $this->q == 'video' || $this->q == 'news' ? 6 : 4;
+        $count = 20;
         $this->count = $count;
         $offset = ($this->page - 1) * $count;
         $this->page_count = ceil($this->all_items_count / $count);
